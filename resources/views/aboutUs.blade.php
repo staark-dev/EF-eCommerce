0be1cb368c72b17c8c5aad455622966cced29b3b
Paul_Tiplea <paul.tiplea2020@gmail.com>
2020-05-19 18:19:16 +0300

    no message

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -1,7 +1,86 @@
-@extends('welcome')
+@extends('layouts.app')
 
 @section( 'content')
-    <div>
-        <h1>Hello</h1>
-    </div>
+
+    <body style="background:white">
+        <div style="border-bottom:1px solid gainsboro">
+        <div class="jumbotron" style="background: white">
+           
+            <div class="card mb-3" style="width:100%;border:none">
+                <img src="https://blog.3dcart.com/hubfs/iStock-1051616786.jpg" class="card-img-top" alt="">
+                <div class="card-body">
+                </div>
+              </div>
+
+           <br>
+           <br>
+            <div class="about-container">
+            <h4 style="color: grey">-About us-</h4>
+            <br>
+            <div id="text">
+            <p align="left">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla quia veniam, doloribus blanditiis dolorum molestias. Ab iste, reprehenderit atque obcaecati natus dicta hic nulla nostrum deleniti sed? Vitae, repellat quos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est natus rerum quibusdam et? Magni porro rerum quisquam suscipit? Illum minima odit ratione doloribus, nostrum minus explicabo cupiditate porro quasi. Accusamus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum minima cum facere, assumenda nemo maxime iste, dignissimos voluptatum explicabo ab sint molestias a consequuntur perferendis in adipisci quas? Odit, exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo porro, facere molestias nobis dolorum eaque vitae doloremque natus mollitia ullam repudiandae placeat consectetur maxime omnis aperiam, qui nemo/p>
+            </div>
+           
+            <div id="text">
+                <p align="left" style="padding-bottom:100px">m facere, assumenda nemo maxime iste, dignissimos voluptatum explicabo ab sint molestias a consequuntur perferendis in adipisci quas? Odit, exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo porro, facere molestias nobis dolorum eaque vitae doloremque natus mollitia ullam repudiandae placeat consectetur maxime omnis aperiam, qui nemo aliquid eum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ea delectus nobis consectetur excepturi. Voluptates enim nihil, voluptatem eum laudantium corrupti nesciunt delectus minus aspernatur, fugit cupiditate illum ratione excepturi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum magnam exercitationem possimus fugit velit quidem est harum molestiae. Rerum quidem facilis beatae fuga harum maxime corporis veritatis. Minima, provident cumque?/p>
+                </div>
+
+                <br>
+
+
+
+          </div>
+            <div class="footerPage" style="width: 100%; border:none">
+                <img src="https://wp-en.oberlo.com/wp-content/uploads/2019/09/What-is-eCommerce.png" alt="">
+            </div>
+            <div class="about-container">
+                <br>
+                <br>
+                <br>
+                <div id="text">
+                <p align="left">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla quia veniam, doloribus blanditiis dolorum molestias. Ab iste, reprehenderit atque obcaecati natus dicta hic nulla nostrum deleniti sed? Vitae, repellat quos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est natus rerum quibusdam et? Magni porro rerum quisquam suscipit? Illum minima odit ratione doloribus, nostrum minus explicabo cupiditate porro quasi. Accusamus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum minima cum facere, assumenda nemo maxime iste, dignissimos voluptatum explicabo ab sint molestias a consequuntur perferendis in adipisci quas? Odit, exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo porro, facere molestias nobis dolorum eaque vitae doloremque natus mollitia ullam repudiandae placeat consectetur maxime omnis aperiam, qui nemo/p>
+                </div>
+                <br>
+                <div id="text">
+                    <p align="left">m facere, assumenda nemo maxime iste, dignissimos voluptatum explicabo ab sint molestias a consequuntur perferendis in adipisci quas? Odit, exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo porro, facere molestias nobis dolorum eaque vitae doloremque natus mollitia ullam repudiandae placeat consectetur maxime omnis aperiam, qui nemo aliquid eum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ea delectus nobis consectetur excepturi. Voluptates enim nihil, voluptatem eum laudantium corrupti nesciunt delectus minus aspernatur, fugit cupiditate illum ratione excepturi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum magnam exercitationem possimus fugit velit quidem est harum molestiae. Rerum quidem facilis beatae fuga harum maxime corporis veritatis. Minima, provident cumque?/p>
+                    </div>
+
+            </div>
+            <div class="footerPage" style="width: 100%; border:none;">
+                <img src="https://odoocdn.com/web/image/slide.channel/26/image_1920/eCommerce?unique=4677b1f" style="width:880px;height:450px" alt="">
+            </div>
+            <div class="about-container">
+                <br>
+                <br>
+                <br>
+                <div id="text">
+                <p align="left">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla quia veniam, doloribus blanditiis dolorum molestias. Ab iste, reprehenderit atque obcaecati natus dicta hic nulla nostrum deleniti sed? Vitae, repellat quos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est natus rerum quibusdam et? Magni porro rerum quisquam suscipit? Illum minima odit ratione doloribus, nostrum minus explicabo cupiditate porro quasi. Accusamus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum minima cum facere, assumenda nemo maxime iste, dignissimos voluptatum explicabo ab sint molestias a consequuntur perferendis in adipisci quas? Odit, exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo porro, facere molestias nobis dolorum eaque vitae doloremque natus mollitia ullam repudiandae placeat consectetur maxime omnis aperiam, qui nemo/p>
+                </div>
+                <br>
+                <div id="text">
+                    <p align="left">m facere, assumenda nemo maxime iste, dignissimos voluptatum explicabo ab sint molestias a consequuntur perferendis in adipisci quas? Odit, exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo porro, facere molestias nobis dolorum eaque vitae doloremque natus mollitia ullam repudiandae placeat consectetur maxime omnis aperiam, qui nemo aliquid eum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ea delectus nobis consectetur excepturi. Voluptates enim nihil, voluptatem eum laudantium corrupti nesciunt delectus minus aspernatur, fugit cupiditate illum ratione excepturi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum magnam exercitationem possimus fugit velit quidem est harum molestiae. Rerum quidem facilis beatae fuga harum maxime corporis veritatis. Minima, provident cumque?/p>
+                    </div>
+
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            
+          
+    </body>
 @endsection
+
+<style>
+
+    .jumbotron{
+        margin-left: 20%;
+        margin-right: 20%;
+        text-align: center
+    }
+    .about-container{
+        margin-right: 15%;
+        margin-left:15%;
+    }
+    
+</style>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- this is contact page --}}
+    <body>
+        <div id="container">
+            <div class="jumbotron" style="background: white">
+                <form>
+                    <div class="form-group row">
+                      <label for="staticEmail" class="col-sm-2 col-form-label">Email:</label>
+                      <div class="col-sm-10">
+                        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="inputPassword" class="col-sm-2 col-form-label">Adress:</label>
+                      <div class="col-sm-10">
+                        1 N. Lyme Drive
+                        Champlin, MN 55316
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Fax: </label>
+                        <div class="col-sm-10">
+                            1-408-999 8888
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Phone number: </label>
+                        <div class="col-sm-10">
+                            +1-555-2248-043
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Social Media: </label>
+                        <div class="col-sm-10">
+                           <i class="fa fa-facebook-square"></i> <i style="margin-left:10px" class="fa fa-instagram"></i> <i style="margin-left:10px" class="fa fa-twitter"></i> <i style="margin-left:10px" class="fa fa-youtube"></i>
+                        </div>
+                      </div>
+                      
+                  </form>
+              </div>
+        </div>
+    </body>
 @endsection
+
+<style>
+    body{
+        background: url("https://www.kindpng.com/picc/m/107-1074254_abstract-world-map-transparent-background-abstract-world-map.png")
+    }
+    .jumbotron{
+        margin-left:15%;
+        margin-right: 15%;
+    }
+   
+</style>
